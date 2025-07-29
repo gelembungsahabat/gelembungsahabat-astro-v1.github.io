f2b35cad4b8847e3aee2c9c37ece6dfd851b7783
gelembungsahabat <wildanrgssport@gmail.com>
2025-07-30 00:27:49 +0700

    feat: can show hero section, at least

---
+++ src/components/App.tsx
@@ -0,0 +1,88 @@
+import "./App.css";
+import { type WheelEvent, useState, useEffect, useRef } from "react";
+import { SectionBar } from "./section-bar";
+import { AnimeRecommendation, Articles, Hero, HiraganaQuiz } from "./sections";
+
+import { usePreventMousewheelZoom } from "../hooks";
+
+function App() {
+  const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
+  const startScreenYRef = useRef(0);
+  const maxSectionIndex = 3;
+
+  const scroll = (e: WheelEvent<Element>) => {
+    if (e.deltaY >= 1 && currentSectionIndex < maxSectionIndex) {
+      setCurrentSectionIndex((prevState) => prevState + 1);
+    }
+    if (e.deltaY <= 1 && currentSectionIndex >= 1) {
+      setCurrentSectionIndex((prevState) => prevState - 1);
+    }
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    startScreenYRef.current = e.touches[0].screenY;
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    const endScreenY = e.changedTouches[0].screenY;
+    const startScreenY = startScreenYRef.current;
+    if (startScreenY < endScreenY && currentSectionIndex >= 1) {
+      setCurrentSectionIndex((prevState) => prevState - 1);
+    }
+    if (startScreenY > endScreenY && currentSectionIndex < maxSectionIndex) {
+      setCurrentSectionIndex((prevState) => prevState + 1);
+    }
+  };
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    // Arrow key scrolling
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (["ArrowUp"].includes(e.code) && currentSectionIndex >= 1) {
+        e.preventDefault();
+        setCurrentSectionIndex((prevState) => prevState - 1);
+      }
+      if (
+        ["ArrowDown"].includes(e.code) &&
+        currentSectionIndex < maxSectionIndex
+      ) {
+        e.preventDefault();
+        setCurrentSectionIndex((prevState) => prevState + 1);
+      }
+    };
+    window.addEventListener("keydown", onKeyDown, false);
+
+    // Scroll to section
+    setTimeout(() => {
+      sections[currentSectionIndex].scrollIntoView({ behavior: "smooth" });
+    }, 100);
+
+    // Prevent KeyDown being triggered 2 times
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [currentSectionIndex, setCurrentSectionIndex]);
+
+  usePreventMousewheelZoom();
+
+  return (
+    <>
+      <div
+        className="homepage"
+        onWheel={scroll}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <Hero setCurrentSection={setCurrentSectionIndex} />
+        <AnimeRecommendation />
+        <HiraganaQuiz />
+        <Articles />
+      </div>
+      <SectionBar
+        currentSection={currentSectionIndex}
+        setCurrentSection={setCurrentSectionIndex}
+      />
+    </>
+  );
+}
+
+export default App;
