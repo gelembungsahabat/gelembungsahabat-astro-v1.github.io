d83ef95903e9b8fbd36ae00f7b012676cbdb4c35
gelembungsahabat <wildanrgssport@gmail.com>
2025-08-05 05:40:30 +0700

    feat: add intro content for aeromodelling article

---
+++ src/components/pages/basic-aeromodelling.tsx
@@ -0,0 +1,23 @@
+import "./styles/article.css";
+import { CodeBlock } from "../code-block";
+export function BasicAeromodellingArticle() {
+  return (
+    <div className="content-wrapper">
+      <h1>Basic Aeromodelling</h1>
+      <p>
+        Aeromodelling uses Unmanned Aerial Vehicles (UAV) Technology. Many
+        models are "mini" scale replicas of real-world aircraft, primarily
+        intended for fun.
+      </p>
+      <h2>Type Of Aeromodelling</h2>
+      <p>Plane Model</p>
+      <p>Glider Model</p>
+      <p>Helicopter Model</p>
+      <p>Quadcopter</p>
+      <p>Hexacopter</p>
+
+      <h2>Plane Model</h2>
+      <p>WIP</p>
+    </div>
+  );
+}
