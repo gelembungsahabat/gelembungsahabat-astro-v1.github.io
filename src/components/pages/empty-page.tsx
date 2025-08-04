0788d8e327f65083c7937cbb04a01e14307faba1
gelembungsahabat <wildanrgssport@gmail.com>
2025-08-04 08:28:59 +0700

    feat: add empty page for wip articles

---
+++ src/components/pages/empty-page.tsx
@@ -0,0 +1,10 @@
+import "./styles/article.css";
+import { CodeBlock } from "../code-block";
+export function EmptyPage() {
+  return (
+    <div className="content-wrapper">
+      <h1>Empty Page</h1>
+      <p>This page still in development, please be patient :D</p>
+    </div>
+  );
+}
