5a9faf2b711cc3684c494a307c888c838b191f7d
gelembungsahabat <wildanrgssport@gmail.com>
2025-08-14 06:51:36 +0700

    feat: add anime drawing article

---
+++ src/components/pages/anime-drawing.tsx
@@ -0,0 +1,23 @@
+import "./styles/article.css";
+import { CodeBlock } from "../code-block";
+import { ArticleWrapper } from "./article-wrapper";
+export function AnimeDrawingArticle() {
+  return (
+    <ArticleWrapper>
+      <h1>Anime Drawing</h1>
+      <p>
+        This time, we will learn about how to drawing <code>Anime like</code>{" "}
+        style character. You don't need fancy tools or something, one of the
+        most important thing that you need to draw Anime character is your{" "}
+        <code>hand</code> and <code>imagination</code>.
+      </p>
+      <br />
+      <h2>Imagination Implementation</h2>
+      <p>
+        The most difficult part of drawing is implement your imagination to your
+        drawing paper. because of it, you need to know exactly what is your
+        object details.
+      </p>
+    </ArticleWrapper>
+  );
+}
