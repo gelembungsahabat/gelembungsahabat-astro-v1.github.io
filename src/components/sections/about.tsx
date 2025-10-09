cc6682bbd8be26d5afc4ba1083c9c6e5dd90aee0
gelembungsahabat <wildanrgssport@gmail.com>
2025-10-09 16:06:08 +0700

    temporarily remove about div

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -5,14 +5,6 @@ export function About() {
   return (
     <section className="about">
       <div className="about-container">
-        <h1>About</h1>
-        <p>
-          I’m a Software Engineer passionate about crafting accessible,
-          pixel-perfect user interfaces that blend thoughtful design with robust
-          engineering.
-        </p>
-      </div>
-      <div className="about-container">
         <h1>Experience</h1>
         <ul className="experience-list">
           <li>
