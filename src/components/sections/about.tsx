cb5b98dfb09f0a7cf0f3bdc966822a4bc1e7500d
gelembungsahabat <wildanrgssport@gmail.com>
2025-10-09 15:16:57 +0700

    feat: add unstyled experience on about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -12,6 +12,17 @@ export function About() {
           engineering.
         </p>
       </div>
+      <div className="about-container">
+        <h1>Experience</h1>
+        <ul>
+          <li>Product Engineer Zero One Group | Apr 2021 - Mar 2025</li>
+          <li>
+            Front-End Developer Aegis E-Sport Academy | Jan 2021 - Apr 2021
+          </li>
+          <li>Web Developer Nanas Media | Nov 2020 - Dec 2020</li>
+          <li>Internship Aegis E-Sport Academy | Dec 2019 - Mar 2020</li>
+        </ul>
+      </div>
     </section>
   );
 }
