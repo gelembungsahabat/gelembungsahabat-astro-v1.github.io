0ea5767d430468d09321fda0f2924c46116626bc
gelembungsahabat <wildanrgssport@gmail.com>
2025-10-11 00:44:03 +0700

    feat: make articles paginated to make them easier to navigate on mobile

--- src/components/sections/articles.tsx
+++ src/components/sections/articles.tsx
@@ -1,22 +1,63 @@
 import "./styles/articles.css";
 import { articleListData } from "../../data";
+import { useState } from "react";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import { motion } from "motion/react";
 
 export function Articles() {
+  const [pagination, setPagination] = useState(0);
   return (
     <section className="articles">
       <h1>Fun Articles* </h1>
       <h2>[Still on Progress]</h2>
       <div className="articles-container">
-        {articleListData.map((val, idx) => {
-          return (
-            <a href={val.href} className="article-card" key={idx}>
-              <div className="content-wrapper">
-                <h1 className="title">{val.title}</h1>
-                <h2 className="subtitle">{val.subtitle}</h2>
-              </div>
-            </a>
-          );
-        })}
+        <button
+          className="transparent-button"
+          disabled={pagination === 0}
+          onClick={() =>
+            setPagination((prev) => {
+              if (prev > 0) {
+                return prev - 1;
+              } else {
+                return prev;
+              }
+            })
+          }
+        >
+          <IoIosArrowUp />
+        </button>
+        {articleListData
+          .map((val, idx) => {
+            return (
+              <a href={val.href} className="article-card" key={idx}>
+                <motion.div
+                  className="content-wrapper"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6, ease: "easeOut" }}
+                >
+                  <h1 className="title">{val.title}</h1>
+                  <h2 className="subtitle">{val.subtitle}</h2>
+                </motion.div>
+              </a>
+            );
+          })
+          .slice(0 + pagination, 4 + pagination)}
+        <button
+          className="transparent-button"
+          disabled={pagination === 4}
+          onClick={() =>
+            setPagination((prev) => {
+              if (prev < 4) {
+                return prev + 1;
+              } else {
+                return prev;
+              }
+            })
+          }
+        >
+          <IoIosArrowDown />
+        </button>
       </div>
     </section>
   );
